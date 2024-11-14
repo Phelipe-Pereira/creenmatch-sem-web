e64cce06b9f803e8516fb3836d51f4a907a16fb5
Phelipe Pereira <phelipe.pereira0811@hotmail.com>
2024-11-14 18:59:27 -0300

    Realizando modelagem dos dados.

--- src/main/java/br/com/alura/screenmatch/service/ConsumoApi.java
+++ src/main/java/br/com/alura/screenmatch/service/ConsumoApi.java
@@ -7,13 +7,13 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ConsumoApi {
-    public String obterDados(String endereco){
+
+    public String obterDados(String endereco) {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = null;
-
         try {
             response = client
                     .send(request, HttpResponse.BodyHandlers.ofString());
