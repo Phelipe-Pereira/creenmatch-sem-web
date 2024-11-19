ec73a6de14f37c778803ec8bc3b378180ed90ed7
Phelipe Pereira <phelipe.pereira0811@hotmail.com>
2024-11-19 19:24:52 -0300

    Inserindo atributos e comportamentos para classe Princial

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,6 +1,21 @@
 package br.com.alura.screenmatch.principal;
 
-public class Principal {
-    public void exibeMenu(){}
+import br.com.alura.screenmatch.service.ConsumoApi;
 
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner leitura = new Scanner(System.in);
+
+    private ConsumoApi consumoApi = new ConsumoApi();
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=8f410fb1";
+
+    public void exibeMenu(){
+        System.out.println("Escreva o nome da série para busca: ");
+        var nomeSerie = leitura.nextLine();
+
+        var json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+    }
 }
