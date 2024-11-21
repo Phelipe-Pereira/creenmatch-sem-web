727067ab0e79350e9971ad99961f9520e6ca3adb
Phelipe Pereira <phelipe.pereira0811@hotmail.com>
2024-11-21 18:56:40 -0300

    Removendo comportamentos da classe app e realocando para classe main.

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,21 +1,37 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
 
     private Scanner leitura = new Scanner(System.in);
-
     private ConsumoApi consumoApi = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=8f410fb1";
 
     public void exibeMenu(){
         System.out.println("Escreva o nome da série para busca: ");
         var nomeSerie = leitura.nextLine();
-
         var json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        List<DadosTemporada> listaTemporadas = new ArrayList<>();
+
+        for (int i = 0; i < dados.totalTemporadas(); i++){
+            json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            listaTemporadas.add(dadosTemporada);
+        }
+
+        listaTemporadas.forEach(System.out::println);
     }
 }
